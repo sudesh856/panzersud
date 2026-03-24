4d15fa5fa25acb067c5884c6bc229593c556e225
Sudesh <nooneandnothing09@gmail.com>
2026-03-24 22:31:36 +0545

    wire pool to run command, live request counter

--- cmd/run.go
+++ cmd/run.go
@@ -1,8 +1,13 @@
 package cmd
 
 import (
+	"context"
 	"fmt"
+	"time"
+
 	"github.com/spf13/cobra"
+	"github.com/sudesh856/LoadForge/internal/pool"
+	"github.com/sudesh856/LoadForge/internal/worker"
 )
 
 var url  string
@@ -14,8 +19,49 @@ var runCmd = &cobra.Command{
 	Use: "run",
 	Short: "Run a load test",
 	Run:func(cmd *cobra.Command, args []string) {
-		fmt.Printf("Hitting %s with %d VUs for %s\n", url, vus, duration)
+		dur, err := time.ParseDuration(duration)
+		if err != nil {
+			fmt.Println("Invalid duration:", err)
+			return
+		}
 
+		ctx, cancel := context.WithTimeout(context.Background(), dur)
+		defer cancel()
+		p := pool.New(100)
+		p.Start(ctx, vus)
+
+		//submitting jobs until context expires
+
+		go func() {
+			for {
+				select{
+				case <-ctx.Done():
+					return
+				default:
+					p.Submit(worker.Job{URL: url})
+				}
+			}
+		}()
+
+		//collecting results until context expires
+
+		total := 0
+		errors := 0
+
+		for {
+			select {
+			case <-ctx.Done():
+				fmt.Printf("\nDone! Total requests: %d Errors: %d\n", total, errors)
+				return
+			case result := <-p.Results():
+				total++
+				if result.Err != nil {
+					errors++
+				}
+
+				fmt.Printf("\rRequests: %d | Errors: %d", total, errors)
+			}
+		}
 	},
 }
 
